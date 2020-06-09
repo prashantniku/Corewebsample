e0caa956114ce492dd9a45ca4f492153200224b2
prashantniku <52192918+prashantniku@users.noreply.github.com>
2020-06-09 11:30:06 +0530

    Test project added

---
+++ Corewebsample.Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Corewebsample.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace Corewebsample.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void Index1()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
--- Corewebsample/Controllers/HomeController.cs
+++ Corewebsample/Controllers/HomeController.cs
@@ -11,12 +11,12 @@ namespace Corewebsample.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
